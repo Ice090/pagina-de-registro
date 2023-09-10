ecf9094f7a242eef0d15e5dcd3f096ac7a924e60
loraxexe <douglax2005@gmail.com>
2023-09-10 18:55:18 -0300

    commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,5 @@
-function redirecionar(event) {
-    var email = document.getElementById('email')
-    var senha = document.getElementById('senha')
-    var check = document.getElementById('check')
-
-    event.preventDefault();
-    //var email = document.querySelector('input[name="username"]').value;
-    //var senha = document.querySelector('input[name="password"]').value;
-
-    if (email.value.length > 0 && senha.value.length > 0) {
-        if (check.checked) {
-            window.location.href = "https://plataforma-ai.vercel.app";
-        } else {
-            window.alert('aceite os termos de uso antes de se registrar.')
-        }
-    }
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('ativado');
 }
+
